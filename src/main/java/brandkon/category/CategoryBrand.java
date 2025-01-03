5a7550111358f595ae0f19fccf3225ccad9963b3
HeoJae <jea1257@naver.com>
2025-01-03 16:00:54 +0900

    feat. 1:N -> N:M변경 CategoryBrand 엔티티 클래스 생성

---
+++ src/main/java/brandkon/category/CategoryBrand.java
@@ -0,0 +1,35 @@
+package brandkon.category;
+
+
+import brandkon.brand.Brand;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class CategoryBrand {
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+}
